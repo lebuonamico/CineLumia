71c1910057f7fd3b3bb40828d11431d9561659f1
lucas.mrabinovich@gmail.com <lucas.mrabinovich@gmail.com>
2025-11-05 19:42:12 -0300

    Agrego controladores y vistas para consumibles y películas. Se actualiza el layout general y la configuración del proyecto.

---
+++ Cine-Lumia/Controllers/ConsumiblesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Cine_Lumia.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace Cine_Lumia.Controllers
+{
+    public class ConsumiblesController : Controller
+    {
+        private readonly CineDbContext _context;
+
+        public ConsumiblesController(CineDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var consumiblesPorCine = _context.CineConsumibles
+                .Include(cc => cc.Cine)
+                .Include(cc => cc.Consumible)
+                .ToList();
+
+            return View(consumiblesPorCine);
+        }
+    }
+}
---
+++ Cine-Lumia/Controllers/PeliculasController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Cine_Lumia.Models;  
+using System.Linq;
+
+namespace Cine_Lumia.Controllers
+{
+    public class PeliculasController : Controller
+    {
+        private readonly CineDbContext _context;
+
+        public PeliculasController(CineDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+             
+            var peliculas = _context.Peliculas.ToList();
+            return View(peliculas);
+        }
+    }
+}
